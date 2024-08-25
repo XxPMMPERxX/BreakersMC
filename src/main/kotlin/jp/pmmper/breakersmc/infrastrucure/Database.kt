3ae9268c480910df3209e1951f0d59640cb50413
deceitya <49361292+deceitya@users.noreply.github.com>
2024-08-25 17:44:48 +0900

    DBクラス

---
+++ src/main/kotlin/jp/pmmper/breakersmc/infrastrucure/Database.kt
@@ -0,0 +1,32 @@
+package jp.pmmper.breakersmc.infrastrucure
+
+import java.sql.Connection
+import java.sql.DriverManager
+
+object Database {
+    var url: String? = null
+    var id: String? = null
+    var password: String? = null
+    private var connection: Connection? = null
+
+    fun connect(): Connection {
+        if (connection == null || connection?.isClosed == true) {
+            connection = DriverManager.getConnection(url, id, password);
+        }
+        return connection as Connection
+    }
+
+    fun beginTransaction() {
+        connection?.autoCommit = false
+    }
+
+    fun commit() {
+        connection?.commit()
+        connection?.autoCommit = true
+    }
+
+    fun rollback() {
+        connection?.rollback()
+        connection?.autoCommit = true
+    }
+}
