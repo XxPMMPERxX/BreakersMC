11e608d49205eb3ad66669e8a7825faeeceb6b09
deceitya <49361292+deceitya@users.noreply.github.com>
2024-08-25 13:59:05 +0900

    プラグイン起動

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
+    kotlin("jvm") version "1.9.24"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "jp.pmmper"
@@ -7,9 +8,11 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
     testImplementation(kotlin("test"))
 }
 
@@ -18,5 +21,5 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain(21)
 }
---
+++ src/main/kotlin/jp/pmmper/breakersmc/BreakersMCPlugin.kt
@@ -0,0 +1,5 @@
+package jp.pmmper.breakersmc
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class BreakersMCPlugin : JavaPlugin()
