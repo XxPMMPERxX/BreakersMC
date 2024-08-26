59c2622e999446b5a8617a7a0580ac2aabc2b026
deceitya <49361292+deceitya@users.noreply.github.com>
2024-08-27 00:43:23 +0900

    最新の値を取得するメソッド

--- src/main/kotlin/jp/pmmper/breakersmc/domain/account/LevelHistoryRepository.kt
+++ src/main/kotlin/jp/pmmper/breakersmc/domain/account/LevelHistoryRepository.kt
@@ -3,5 +3,7 @@ package jp.pmmper.breakersmc.domain.account
 interface LevelHistoryRepository {
     fun find(account: AccountID): List<LevelHistory>
 
+    fun findLatestLevel(account: AccountID): Level
+
     fun store(history: LevelHistory)
 }
--- src/main/kotlin/jp/pmmper/breakersmc/domain/account/MoneyHistoryRepository.kt
+++ src/main/kotlin/jp/pmmper/breakersmc/domain/account/MoneyHistoryRepository.kt
@@ -3,5 +3,7 @@ package jp.pmmper.breakersmc.domain.account
 interface MoneyHistoryRepository {
     fun find(account: AccountID): List<MoneyHistory>
 
+    fun findLatestMoney(account: AccountID): Money
+
     fun store(history: MoneyHistory)
 }
