2d2b3c81fc5450b3624820b5200acd487dcf2afb
deceitya <49361292+deceitya@users.noreply.github.com>
2024-09-18 22:43:18 +0900

    IDの0以上の担保

--- src/main/kotlin/jp/pmmper/breakersmc/domain/account/AccountID.kt
+++ src/main/kotlin/jp/pmmper/breakersmc/domain/account/AccountID.kt
@@ -1,3 +1,7 @@
 package jp.pmmper.breakersmc.domain.account
 
-data class AccountID(val value: Int)
+data class AccountID(val value: Int) {
+    init {
+        require(value >= 0)
+    }
+}
--- src/main/kotlin/jp/pmmper/breakersmc/domain/game/GameID.kt
+++ src/main/kotlin/jp/pmmper/breakersmc/domain/game/GameID.kt
@@ -1,3 +1,7 @@
 package jp.pmmper.breakersmc.domain.game
 
-data class GameID(val value: Int)
+data class GameID(val value: Int) {
+    init {
+        require(value >= 0)
+    }
+}
