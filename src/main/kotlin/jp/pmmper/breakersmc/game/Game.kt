a0d4c02a2ec1cd1373366fbfc5f8e1a8da1a00bc
deceitya <49361292+deceitya@users.noreply.github.com>
2024-09-20 00:50:42 +0900

    Game自体はプレイヤー必要なし

--- src/main/kotlin/jp/pmmper/breakersmc/game/Game.kt
+++ src/main/kotlin/jp/pmmper/breakersmc/game/Game.kt
@@ -2,54 +2,16 @@ package jp.pmmper.breakersmc.game
 
 import jp.pmmper.breakersmc.event.PhaseChangingEvent
 import org.bukkit.Bukkit
-import org.bukkit.entity.Player
 import java.time.LocalDateTime
 
 abstract class Game(id: GameID) {
     val id: GameID = id
-    val players = mutableListOf<Player>()
     var beginAt: LocalDateTime? = null
         protected set
     var endedAt: LocalDateTime? = null
         protected set
     var phase = Phase.CLOSE
         private set
-
-    /**
-     * プレイヤーがゲームに参加しているか
-     *
-     * @param player
-     * @return
-     */
-    fun isPlayerJoined(player: Player): Boolean {
-        return players.contains(player)
-    }
-
-    /**
-     * プレイヤーを参加させる
-     *
-     * @param player
-     * @return
-     */
-    fun addPlayer(player: Player): Boolean {
-        if (!isPlayerJoined(player)) {
-            return players.add(player)
-        }
-        return false
-    }
-
-    /**
-     * プレイヤーを退出させる
-     *
-     * @param player
-     * @return
-     */
-    fun kickPlayer(player: Player): Boolean {
-        if (isPlayerJoined(player)) {
-            return players.remove(player)
-        }
-        return false
-    }
 
     /**
      * 次フェーズに移行
